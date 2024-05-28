7db00a029db8cf17a7dbfd2f07a3af43392a11c9
borntofall <vsexobnyal@yandex.ru>
2024-05-28 18:42:01 +0600

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int money = 15000;
         int total = 0;
         int month = 0;
-        while ( total < 2_459_000 ) {
+        while (total < 2_459_000) {
             total = total + money;
             total = total + total / 100;
             System.out.println("Месяц " + month++ + " , сумма накоплений равна " + total + " рублей");
@@ -15,34 +15,34 @@ public class Main {
 
         System.out.println("Задача 2");
 
-        int q = 0;
-        while ( q < 10 ) {
-            q = q + 1;
-            System.out.printf(" %s ", q);
+        int countdown = 0;
+        while (countdown < 10) {
+            countdown = countdown + 1;
+            System.out.printf(" %s ", countdown);
         }
 
         System.out.println("\n");
 
-        int i = 10;
-        for (i = 10; i >= 1; i-- ) {
 
-            System.out.printf(" %s " , i);
+        for ( int i = 10; i >= 1; i--) {
+
+            System.out.printf(" %s ", i);
         }
         System.out.println("\n");
 
 
         System.out.println("Задача 3");
 
-        int y = 12_000_000;
+        int population = 12_000_000;
         int birthRate = 17;
-        int dead = 8 ;
+        int dead = 8;
         int year = 1;
 
         while (year <= 10) {
-            int birthRateYear = y /1000 * birthRate;
-            int deadYear = y /1000 * dead;
-            y = y + birthRateYear - deadYear;
-            System.out.println("Год " + year++ + " , численность населения составялет " + y );
+            int birthRateYear = population / 1000 * birthRate;
+            int deadYear = population / 1000 * dead;
+            population = population + birthRateYear - deadYear;
+            System.out.println("Год " + year++ + " , численность населения составялет " + population);
         }
 
         System.out.println("Задача 4");
@@ -50,11 +50,12 @@ public class Main {
         int contribution = 15000;
         total = 0;
         month = 0;
-        while ( month > 0 ) {
+
+        while (month > 0) {
         }
 
         while (total <= 12_000_000) {
-            total = total + contribution;
+            total += contribution;
             total = total + (total * 7) / 100;
             System.out.println("За " + month++ + " месяц сумма накоплений равна " + total);
         }
@@ -65,19 +66,16 @@ public class Main {
         total = 0;
         month = 1;
 
-            while (total < 12_000_000) {
+        while (total < 12_000_000) {
 
-                total = total + contribution;
-                total = total + (total * 7) / 100;
-                if (month % 6 == 0)
-                {
-                    System.out.println("За месяц " + month + " сумма накоплений равна " + total);
-                }
-                month++;
+            total += contribution;
+            total = total + (total * 7) / 100;
+            if (month % 6 == 0) {
+                System.out.println("За месяц " + month + " сумма накоплений равна " + total);
+            }
+            month++;
 
-           }
-
-
+        }
 
 
         System.out.println("Задача 6");
@@ -89,11 +87,10 @@ public class Main {
         int monthInYear = 12;
         int monthTotal = year * monthInYear;
 
-        while (month <= monthTotal ) {
-            total = total + contribution;
+        while (month <= monthTotal) {
+            total  += contribution;
             total = total + (total * 7) / 100;
-            if (month % 6 == 0)
-            {
+            if (month % 6 == 0) {
                 System.out.println("За " + month + " месяцев ,сумма накоплений равна " + total);
             }
             month++;
@@ -103,20 +100,22 @@ public class Main {
 
         int friday = 5;
 
-        while ( friday <= 31) {
-            if (friday % 5 == 0)
-                System.out.println("Сегодня пятница, " + friday + " -е число. Необходимо подготовить отчет" );
+        while (friday <= 31) {
+
+            System.out.println("Сегодня пятница, " + friday + " -е число. Необходимо подготовить отчет");
             friday += 5;
+
         }
 
         System.out.println("Задача 8");
 
-        int a = 2024;
-        int yearBefore = a - 200;
-        int yearAfter = a + 100;
+        int yearNow = 2024;
+        int yearBefore = yearNow - 200;
+        int yearAfter = yearNow + 100;
+        int cometFliesBy = 79;
 
         for (year = yearBefore; year <= yearAfter; year++) {
-            if (year % 79 == 0) {
+            if (year % cometFliesBy == 0) {
                 System.out.println(year);
             }
         }
